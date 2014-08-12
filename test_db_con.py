4be437cf6c508745aa166c06bb34c63dbfc50b91
bdemetris <surfninja@gmail.com>
2014-08-12 15:45:49 -0700

    Initial db test script commit

---
+++ test_db_con.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#this is a test script that returns the database version on a successful connection
+
+import MySQLdb
+
+# Open database connection
+db = MySQLdb.connect("localhost","brett","password","mlpdata" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# execute SQL query using execute() method.
+cursor.execute("SELECT VERSION()")
+
+# Fetch a single row using fetchone() method.
+data = cursor.fetchone()
+
+print "Database version : %s " % data
+
+# disconnect from server
+db.close()
