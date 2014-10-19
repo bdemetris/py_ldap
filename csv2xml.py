f143300f35d9f4723a38e4a74530d4b9f1f37c87
bdemetris <surfninja@gmail.com>
2014-10-19 11:40:30 -0700

    post response handling

--- csv2xml.py
+++ csv2xml.py
@@ -3,13 +3,14 @@
 # Define Username and Password Information #
 # Define Testmode as True or False         #
 # ---------------------------------------- #
-mlp_id = 'username'
-mlp_pw = 'password'
+mlp_id = '17204'
+mlp_pw = 'xml40271api'
 testmode = True
 
 # Libraries used by MLP Script
 import csv
 import requests
+import xml.dom
 
 # target file.  destination file will be created
 csvFile = 'C:\work\work.csv'
@@ -22,7 +23,7 @@ xmlData = open(xmlFile, 'w')
 # write root tag
 xmlData.write('<root>' + "\n")
 
-# write line 2 - customer id and password - testmode?
+# write line 2 - customer id and password
 if testmode == True:
     xmlData.write("<customer id='%s' password='%s' testmode='1'>" % (mlp_id,mlp_pw) + "\n")
 else:
@@ -30,17 +31,15 @@ else:
 
 # parse csv and create the body - actions to take = _Add, _Update
 rowNum = 0
-refNum = 0
+refNum = 25986
 for row in csvData:
     if rowNum == 0:
         tags = row
-        # replace spaces w/ underscores in tag names
-        for i in range(len(tags)):
-            tags[i] = tags[i].replace(' ', '_')
+        # put tagline cleanup code here
     else:
         refNum += 1
         action = 'User_Add'
-        xmlData.write("<action type='%s' ref=%r>" % (action,str(refNum))+ "\n")
+        xmlData.write("<action type='%s' ref='%s'>" % (action,str(refNum))+ "\n")
         for i in range(len(tags)):
             xmlData.write('    ' + '<' + tags[i] + '>' \
                           + row[i] + '</' + tags[i] + '>' + "\n")
@@ -51,18 +50,31 @@ for row in csvData:
 xmlData.write('</customer>' + "\n")
 xmlData.write('</root>' + "\n")
 
+# closing xml file object
+xmlData.close()
 
 # MLP Server POST Request
 
 headers = {'Content-Type': 'text/html'}
 url = 'https://www.mylearningplan.com/xml/dataexchange.asp'
+xml = open(xmlFile).read()
 
-postRequest = requests.post(url, data=xml, headers=headers, auth=(mlp_id, mlp_pw))
+postResponse = requests.post(url, data=xml, headers=headers, auth=(mlp_id, mlp_pw))
+
+#Convert Unicode to String
+stringResponse = str(postResponse.text)
+
+#Split the string into a list.  Assign ref and code to variables, and clean them up 
+refID = stringResponse.split()[3].replace("'","").strip('ref=')
+refCode = stringResponse.split()[4].replace("'","").strip('code=').strip('>Testmode')
+
+print refID, refCode
 
 
-# debugging
-print postRequest.text
+#write response blob to xml file and iterate
+#xmlResponse = 'C:\work\Response.xml'
+#xmlPush = open(xmlResponse, 'w')
+#xmlPush.write(postResponse.text)
+#xmlPush.close()
 
-# closing xml file object
-xmlData.close()
 
